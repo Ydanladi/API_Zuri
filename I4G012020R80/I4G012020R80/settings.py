9f220a7d9b7af25ead151aa9ba3051ae8ea326d1
Ydanladi <105249846+Ydanladi@users.noreply.github.com>
2022-07-17 20:10:27 +0100

    Update settings.py

--- I4G012020R80/I4G012020R80/settings.py
+++ I4G012020R80/I4G012020R80/settings.py
@@ -53,13 +53,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'I4G012020R80.urls'
 
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
+# REST_FRAMEWORK = {
+#     # Use Django's standard `django.contrib.auth` permissions,
+#     # or allow read-only access for unauthenticated users.
+#     #'DEFAULT_PERMISSION_CLASSES': [
+#         #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+#     ]
+# }
 
 
 TEMPLATES = [
